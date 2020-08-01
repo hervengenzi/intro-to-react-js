2c072cc486dd387ec1ede531b517dcaa08bbc288
hervengenzi <ngenziherve@octangrp.com>
2020-08-01 08:48:31 +0200

    Using props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Person from './Person/Person';
 class App extends Component {
   render(){
   return (
     <div className="App">
       <h1>I am a robot</h1>
+      <Person name="Herve" age="29" />
+      <Person name="Fidele" age="30"/>
+      <Person name="Ngenzi" age="40"/>
     </div>
+    
   );
     // return React.createElement('div', { className: 'App'}, React.createElement('h1', null, 'I am a robot'));
   }
---
+++ src/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const person = (props) => {
+return <p>I am {props.name} and i have {props.age}</p>
+}
+
+
+export default person;
